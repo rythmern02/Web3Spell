1fee81c1111ad8e71a7e16ee30419e0f212fb140
Rythme Nagrani <117756525+rythmern02@users.noreply.github.com>
2024-06-06 15:13:16 +0530

    Update Courses.tsx

--- components/main/Courses.tsx
+++ components/main/Courses.tsx
@@ -13,7 +13,7 @@ const Courses = () => {
       <div className="h-full w-full flex flex-col md:flex-row gap-10 px-10">
       
         <CourseCard
-          src="/Wormhole.jpg"
+          src="/WormholeToken.jpg"
           title="Modern Next.js Portfolio"
           description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
         />
