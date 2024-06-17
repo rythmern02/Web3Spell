1461809d711e9def67bc2b6313baaded470a78d0
Rythme Nagrani <117756525+rythmern02@users.noreply.github.com>
2024-06-17 12:54:32 +0530

    Create page.tsx

---
+++ app/(auth)/sign-up/[[...sign-up]]/page.tsx
@@ -0,0 +1,9 @@
+import { SignUp } from "@clerk/nextjs";
+
+export default function Page() {
+  return (
+    <div className="my-24 flex justify-center items-center relative z-20">
+      <SignUp/>
+    </div>
+  );
+}
