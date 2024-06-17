6ce144e2df933f6fbb878c8d5abb147c67ab5818
Rythme Nagrani <117756525+rythmern02@users.noreply.github.com>
2024-06-17 12:52:41 +0530

    Update page.tsx

--- app/(auth)/sign-in/[[...sign-in]]/page.tsx
+++ app/(auth)/sign-in/[[...sign-in]]/page.tsx
@@ -1,1 +1,10 @@
+import { SignIn } from "@clerk/nextjs";
+
+export default function Page() {
+  return (
+    <div className="my-24 flex justify-center items-center relative z-20">
+      <SignIn />
+    </div>
+  );
+}
 
