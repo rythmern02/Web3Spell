5419f20a807249561f72e39f1a5c6f3319dc9b8c
Rythme Nagrani <117756525+rythmern02@users.noreply.github.com>
2024-06-25 00:08:23 +0530

    Add files via upload

---
+++ components/main/Hero.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import HeroContent from "../sub/HeroContent";
+
+const Hero = () => {
+  
+  return (
+    <div className="relative flex flex-col h-full w-full" id="Hero">
+      <HeroContent />
+    </div>
+  );
+};
+
+export default Hero;
