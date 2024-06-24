408048c3087fd4ad1b1efc2c7dafdc2a95f88e63
Rythme Nagrani <117756525+rythmern02@users.noreply.github.com>
2024-06-24 23:58:52 +0530

    updating.....

---
+++ Course/_components/CategoryFilter.tsx
@@ -0,0 +1,55 @@
+"use client"
+import React, { useState } from 'react'
+
+const CategoryFilter = () => {
+    const [activeState, setActiveState] = useState(0)
+    const itemslist = [
+        {
+            id:1,
+            name:"all",
+            value:"all"
+        },
+        {
+            id:2,
+            name:"Blockchain Basics",
+            value:"Basics"
+        },
+        {
+            id:3,
+            name:"Intro to Solana",
+            value:"Solana info"
+        },
+        {
+            id:4,
+            name:"Solana Development",
+            value:"Solana Development"
+        },
+        {
+            id:5,
+            name:"Advance Development",
+            value:"advance Development"
+        }
+        
+    ]
+  return (
+    <div className="flex gap-5 flex-col sm:flex-row sm:justify-center">
+  {itemslist.map((item, index) => (
+    <button
+      key={index}
+      onClick={() => setActiveState(index)}
+      className="
+        border border-blue-900
+        p-2 px-4 text-sm
+        text-white rounded-md
+        hover:border-purple-600 font-semibold
+        active:border-purple-300
+      "
+    >
+      <h2>{item.name}</h2>
+    </button>
+  ))}
+</div>
+  )
+}
+
+export default CategoryFilter;
---
+++ Course/page.jsx
@@ -0,0 +1,51 @@
+"use client"
+import React, { useEffect, useState } from 'react'
+import { getCourseList } from '../_services'
+import Link from 'next/link'
+import CategoryFilter from './_components/CategoryFilter'
+import CourseCard from './_components/CourseCard'
+
+const Page = () => {
+  
+  const [Course, setCourse] = useState([]);
+
+  useEffect(() => {
+    getCourses();
+  }, [])
+  
+  const getCourses = () => {
+    getCourseList().then((res) => { 
+      console.log(res);
+      setCourse(res.courseLists);
+    });
+  };
+
+  return (
+    <div className=' relative z-20 md:mt-16'>
+      <div className="flex flex-col items-center justify-center " id="Courses">
+      <h1 className="text-[40px] font-semibold text-transparent bg-clip-text bg-gradient-to-r from-purple-500 to-cyan-500 py-5">
+        Our Courses
+      </h1>
+      <div  className='flex flex-col'>
+      <CategoryFilter />
+      </div>
+      {Course.length > 0 && (
+        <div className="h-full w-full grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-5 py-5 px-10">
+          {Course.map((course) => (
+            <Link key={course.id} href={"/course-preview/" + course.id}>
+              <CourseCard
+                key={course.id} 
+                src={course.banner.url}
+                title={course.name}
+                description={course.description}
+              />
+            </Link>
+          ))}
+        </div>
+      )}
+    </div>
+    </div>
+  )
+}
+
+export default Page
