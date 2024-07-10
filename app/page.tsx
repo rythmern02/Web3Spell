b5a14e8380bf9f3f96307cb5e8cde374e993c1c9
Rythme Nagrani <117756525+rythmern02@users.noreply.github.com>
2024-07-10 16:10:33 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -2,13 +2,14 @@
 import Hero from "@/components/main/Hero";
 import Page from "./Course/page";
 import Skills from "@/components/main/Skills";
-
-
+import VideoShowcase from "@/components/main/videoShowcase"
+ 
 export default function Home() {
   return (
     <main className="h-full w-full">
       <div className="flex flex-col gap-20">
         <Hero />
+        <VideoShowcase/>
         <Page/>
         <Skills />
       </div>
