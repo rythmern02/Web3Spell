61693125b2a8696f180e941c59cd64730eac6234
Rythme Nagrani <117756525+rythmern02@users.noreply.github.com>
2024-10-18 21:06:49 +0530

    Update page.jsx

--- app/Guides/page.jsx
+++ app/Guides/page.jsx
@@ -34,6 +34,12 @@ const guides = [
     image: "/securityintro.png", 
     link: "https://github.com/rythmern02/Web3Spell/blob/main/Guides/Best%20practices%20in%20Solana%20Development.pdf"
   },
+    {
+    title: "Wormhole - Bridging the Fragmented Web3 Landscape",
+    description: "Wormhole is a decentralized messaging protocol bridging different blockchains, enhancing interoperability and liquidity in the Web3 ecosystem.",
+    image: "/wormhole.png", 
+    link: "https://github.com/rythmern02/Web3Spell/blob/main/Guides/Best%20practices%20in%20Solana%20Development.pdf"
+  },
 ];
 
 const GuidesPage = () => {
